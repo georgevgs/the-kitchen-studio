36d0631939f3de469edfb0c7b9f4e17e997e668c
George Vagdas <dev@vagdas.eu>
2023-09-03 14:51:11 +0300

    Moved homepage images

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="@astrojs/image/client" />
+
+declare module "*.avif" {
+  const content: string;
+  export default content;
+}
+  
