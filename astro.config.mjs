d616d4592be0fff79cef10618f4cfff027ad87e4
George Vagdas <dev@vagdas.eu>
2024-06-25 22:57:03 +0300

    Packages update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,55 +1,26 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
 import { defineConfig, squooshImageService } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
-// import partytown from "@astrojs/partytown";
 import { SITE } from './src/config.mjs';
 import serviceWorker from "astrojs-service-worker";
 import astroI18next from "astro-i18next";
+import vercel from "@astrojs/vercel/serverless";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const whenExternalScripts = (items = []) =>
-  SITE.googleAnalyticsId
-    ? Array.isArray(items)
-      ? items.map((item) => item())
-      : [items()]
-    : [];
-
-// https://astro.build/config
 export default defineConfig({
   image: {
-    service: squooshImageService(),
+    service: squooshImageService()
   },
   site: SITE.origin,
   base: SITE.basePathname,
   trailingSlash: "ignore",
-  output: "static",
-  integrations: [
-    astroI18next(),
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    sitemap(),
-    mdx(),
-    // ...whenExternalScripts(() =>
-    //   partytown({
-    //     config: {
-    //       forward: ["dataLayer.push"],
-    //     },
-    //   })
-    // ),
-    serviceWorker(),
-  ],
-  markdown: {},
-  vite: {
-    resolve: {
-      alias: {
-        "~": path.resolve(__dirname, "./src"),
-      },
-    },
-  },
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
+  integrations: [astroI18next(), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), sitemap(), mdx(), serviceWorker()],
 });
