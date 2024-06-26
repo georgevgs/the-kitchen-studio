17912a3bd7dcc65547cca1c82ae3336e439256c6
George Vagdas <dev@vagdas.eu>
2024-06-26 18:45:38 +0300

    Vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,28 @@
-import { defineConfig, squooshImageService } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-import mdx from '@astrojs/mdx';
-import { SITE } from './src/config.mjs';
+import {defineConfig, squooshImageService} from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+import {SITE} from "./src/config.mjs";
 import serviceWorker from "astrojs-service-worker";
 import astroI18next from "astro-i18next";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 export default defineConfig({
-  image: {
-    service: squooshImageService()
-  },
-  site: SITE.origin,
-  base: SITE.basePathname,
-  trailingSlash: "ignore",
-  output: 'static',
-  integrations: [astroI18next(), tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), sitemap(), mdx(), serviceWorker()],
+    image: {
+        service: squooshImageService()
+    },
+    site: SITE.origin,
+    base: SITE.basePathname,
+    trailingSlash: "ignore",
+    output: "static",
+    adapter: vercel({
+        webAnalytics: {
+            enabled: true,
+        },
+    }),
+    integrations: [astroI18next(), tailwind({
+        config: {
+            applyBaseStyles: false
+        }
+    }), sitemap(), mdx(), serviceWorker()],
 });
